69d5934e0f1f29df0e5f8c301a7d91f7bb0bc86f
Emily Yu <emily.yu@bellevuecollege.edu>
2020-10-24 20:08:56 -0700

    implemented search functionality

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Nav from './Nav';
 import Main from './Main';
+import CreatePost from './CreatePost';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -12,7 +13,8 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Route exact path="/" component={Main} />
-      <Route exact path="/post" component={Nav} />
+      <Route exact path="/post" component={CreatePost} />
+      <Route exact path="/feed" component={Nav} />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
